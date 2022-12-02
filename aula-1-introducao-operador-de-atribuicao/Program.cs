ab08b9ba29d64b26ca625f9266b67761e16f2e71
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-12-02 09:48:41 -0300

    combinando-operadores

--- aula-1-introducao-operador-de-atribuicao/Program.cs
+++ aula-1-introducao-operador-de-atribuicao/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 
+
 int a = 10;
 int b = 20;
 
 int c = a + b;
 
+//c = c + 5; // 30 + 5
+
+c += 5;
+//c = c + 5; // 30 + 5
+
+//c += 5;
+c *= 5;
+// c = c - 5
+
+
 Console.WriteLine(c);
+
 
