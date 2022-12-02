c883fb5c19315f09bbfb47f7794c8f20e2439871
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-12-02 09:49:48 -0300

    convertendo-tipos-de-variaveis

--- aula-1-introducao-operador-de-atribuicao/Program.cs
+++ aula-1-introducao-operador-de-atribuicao/Program.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 
+//int a = Convert.ToInt32("5");
+int a = int.Parse("5i");
 
+//decimal.Parse();
+//Convert.ToInt64();
+
+Console.WriteLine(a);
+
+
+/*
 int a = 10;
 int b = 20;
 
 int c = a + b;
 
-//c = c + 5; // 30 + 5
+c = c + 5; // 30 + 5
 
 c += 5;
-//c = c + 5; // 30 + 5
+c = c + 5; // 30 + 5
 
-//c += 5;
+c += 5;
 c *= 5;
-// c = c - 5
+c = c - 5
 
 
 Console.WriteLine(c);
 
-
+*/
