4e32b333cd73cb402af06a6d38b0082a2d295806
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-12-06 11:17:58 -0300

    operador-condicional-na-pratica

---
+++ aula-2-operadores-condicionais/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+if(possivelVenda)
+{
+	Console.WriteLine("Venda realizada.");
+}
+else
+{
+	Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque.");
+}
+
