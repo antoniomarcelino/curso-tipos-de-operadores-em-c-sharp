6be07fb3999547e8c20f0dc5c9e0fad06024d93c
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-12-06 11:26:01 -0300

    aprendendo-switch-case

--- aula-2-operadores-condicionais/Program.cs
+++ aula-2-operadores-condicionais/Program.cs
@@ -1,5 +1,65 @@
 ﻿using System;
 
+Console.WriteLine("Digite uma letra");
+string letra = Console.ReadLine();
+
+switch(letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+	    Console.WriteLine("Vogal");
+	    break;
+    default:
+        Console.WriteLine("Não é uma vogal.");
+        break;
+}
+
+/*
+if(letra == "a" ||
+    letra == "e" ||
+    letra == "i" ||
+    letra == "o" ||
+    letra == "u")
+{
+    Console.WriteLine("Vogal");
+}
+else
+{
+    Console.WriteLine("Não é uma vogal.");
+}
+*/ 
+    
+
+/* if(letra == "a")
+{
+    Console.WriteLine("Vogal");
+}
+else if(letra == "e")
+{
+    Console.WriteLine("Vogal");
+}
+else if(letra == "i")
+{
+    Console.WriteLine("Vogal");
+}
+else if(letra == "o")
+{
+    Console.WriteLine("Vogal");
+}
+else if(letra == "u")
+{
+    Console.WriteLine("Vogal");
+}
+else
+{
+    Console.WriteLine("Não é uma vogal.");
+} */
+
+
+/*
 int quantidadeEmEstoque = 10;
 int quantidadeCompra = 4;
 bool possivelVenda = quantidadeEmEstoque > 0 && quantidadeEmEstoque >= quantidadeCompra;
@@ -18,5 +78,6 @@ else
 {
 	Console.WriteLine("Desculpe, não temos a quantidade desejada em estoque.");
 }
+*/
 
 
