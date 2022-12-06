11642ff8a40311cdd84c1f2f6f1fea321cf07402
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-12-06 11:10:48 -0300

    cast-implicito

--- aula-1-introducao-operador-de-atribuicao/Program.cs
+++ aula-1-introducao-operador-de-atribuicao/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 
+// int a = 5;
+// double b = a;
+// long a = long.MaxValue;
+// int b = Convert.ToInt32(a);
+int a = int.MaxValue;
+long b = a;
+Console.WriteLine(b);
+
+
+/*
 int inteiro = 5;
 string a = inteiro.ToString();
 Console.WriteLine(a);
 
-/*
 int a = Convert.ToInt32(null); //0
 int a = int.Parse(null); //Value cannot be null
 Console.WriteLine(a);
